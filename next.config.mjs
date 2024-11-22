192bd1328aa408a62761b11845438de3ca034116
Berke Kara <karaberke001@gmail.com>
2024-11-21 21:09:27 -0600

    hello

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,10 @@ import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-  experimental: {
-    mdxRs: true,
-  },
+	pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+	experimental: {
+		mdxRs: true,
+	},
 };
 
 export default withContentlayer(nextConfig);
